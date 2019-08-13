8e9a0933c9abf298699cddccea3949d6544f1ea0
David Garcia <david.garcia@canonical.com>
2019-08-13 13:25:16 +0200

    Add functional tests

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -1,7 +1,39 @@
 #!/usr/bin/python3
 import unittest
 import zaza.model as model
+import requests as http
+
+def get_prometheus_uri():
+    ip = model.get_status().applications['prometheus-k8s']['public-address']
+    port = 9090
+    return "http://{}:{}".format(ip, port)
+
 
 class BasicDeployment(unittest.TestCase):
-    def test_prometheus(self):
-        pass
+    def test_get_prometheus_uri(self):
+        get_prometheus_uri()
+
+    def test_prometheus_get_series(self):
+        prometheus_uri = get_prometheus_uri()
+        body = http.get('{}/api/v1/series?match[]=up'.format(prometheus_uri))
+        self.assertEqual(body.status_code, 200)
+
+    def test_prometheus_get_labels(self):
+        prometheus_uri = get_prometheus_uri()
+        body = http.get('{}/api/v1/labels'.format(prometheus_uri))
+        self.assertEqual(body.status_code, 200)
+
+    def test_prometheus_get_targets(self):
+        prometheus_uri = get_prometheus_uri()
+        body = http.get('{}/api/v1/targets'.format(prometheus_uri))
+        self.assertEqual(body.status_code, 200)
+
+    def test_prometheus_get_alerts(self):
+        prometheus_uri = get_prometheus_uri()
+        body = http.get('{}/api/v1/alerts'.format(prometheus_uri))
+        self.assertEqual(body.status_code, 200)
+
+    def test_prometheus_get_status_config(self):
+        prometheus_uri = get_prometheus_uri()
+        body = http.get('{}/api/v1/status/config'.format(prometheus_uri))
+        self.assertEqual(body.status_code, 200)
